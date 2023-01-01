f255c618d5122643ea6bc0bcf4e070826666a90b
LetsLearnReact <letlearnreact95@gmail.com>
2023-01-02 00:05:40 +0530

    Second Commit

--- script.js
+++
@@ -1,8 +0,0 @@
-const Re = React.createElement('div',{id:'title'},'Namaste React')
-        const Re1 = React.createElement('div',{id:'hello'},'Namaste JS')
-        const Re2 = React.createElement('div',{id:'hi'},'Namaste Node')
-        const Re3 = React.createElement('div',{id:'bye'},[
-            Re,Re1,Re2
-        ])
-        const Re4 = ReactDOM.createRoot(document.getElementById('root'))
-        Re4.render(Re3)
